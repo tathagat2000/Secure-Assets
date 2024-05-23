da923172047de019644687289dc9c5810836709e
Vansh Marda <vansh.marda@vansh.marda-C02DV2MBMD6N>
2024-05-23 21:32:05 +0530

    made changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,18 +10,14 @@ self.addEventListener('activate', event => {
 self.addEventListener("fetch", (event) => {
     const url = new URL(event.request.url);
     if (event.request.method !== "GET") return;
-    const domain_name_1 = "localhost:4000";
-
-    if (url.host === domain_name_1 && url.pathname.startsWith('/imgs')) {
+    const host = "localhost:4000";
+    if (url.host === host && url.pathname.startsWith('/imgs')) {
 
         const new_url = `/api/signed-asset-url?assetUrl=${encodeURIComponent(url)}`;
         const newRequest = new Request(new_url, event.request);
-        
         event.respondWith(
             (async () => {
                 const response = await fetch(newRequest);
-                console.log('Original Request URL:', event.request);
-                console.log('New Request URL:', newRequest);
                 return response;
             })(),
         );
