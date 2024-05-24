60918a29c6511491fc45003a1285bedd85bba839
Vansh Marda <vansh.marda@vansh.marda-C02DV2MBMD6N>
2024-05-24 18:46:34 +0530

    Added tag array instead of hardcoding /img

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+let tags = [];
+
+
 self.addEventListener('install', event => {
     console.log('Service Worker installing.');
     self.skipWaiting()
@@ -7,22 +10,21 @@ self.addEventListener('activate', event => {
     console.log('Service Worker activating.');
 });
 
+
 self.addEventListener("fetch", (event) => {
     const url = new URL(event.request.url);
     if (event.request.method !== "GET") return;
     const host = "localhost:4000";
-    if (url.host === host && url.pathname.startsWith('/imgs')) {
-
+    if (url.host === host && tags.some(tag => url.pathname.includes(tag))) {
         const new_url = `/api/signed-asset-url?assetUrl=${encodeURIComponent(url)}`;
         const newRequest = new Request(new_url, event.request);
-        event.respondWith(
-            (async () => {
-                const response = await fetch(newRequest);
-                return response;
-            })(),
-        );
+        event.respondWith(fetch(newRequest));
     }
-
 });
 
+self.addEventListener("message", (event) => {
+    if (Array.isArray(event.data)) {
+        tags = event.data;
+    }
+});
 
